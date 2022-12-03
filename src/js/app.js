3dd35b2e52c082aa5f72bfa811469c3a620462bd
Atanas <atanasdraganov77@gmail.com>
2022-12-03 20:43:47 +0200

    5 article elements added

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,25 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  var count = 0;
+  document.body.addEventListener("click", ()=> {
+    const length = 5;
 
+    for(let i=0;i<length; i++){
+      let x = document.createElement("article");
+      count += length;
+
+      x.setAttribute("id", "myArticle" + count);
+      document.body.appendChild(x);
+
+      let text = "some text "+count;
+
+      document.getElementById("myArticle"+ count).innerHTML = text;
+      document
+            .getElementById("myArticle" + count)
+            .classList.add("message");
+    }
+  })
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
